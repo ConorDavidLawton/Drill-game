70d165f96d9bf979bdf0e60bb8484aaab1277839
Conor Lawton <conorlawton1464@gmail.com>
2024-06-29 17:20:08 +0100

    Created functions in terrain script to allow detached islands to be created. Added Base scene with background and world boundaries.

--- Player.gd
+++ Player.gd
@@ -1,12 +1,17 @@
 extends CharacterBody2D
 class_name Player
 
-@export var speed = 50
-@export var rotation_speed = 0.6
+@export var speed = 200
+@export var rotation_speed = 3
 
 var rotation_direction = 0
 var object_hit
 
+func _on_drill_detection_body_entered(body: Node) -> void:
+	if body.is_in_group("Player"): return
+	object_hit = body
+	drill()
+	
 func drill():
 	if object_hit.is_in_group("Destructibles"):
 		object_hit.get_parent().clip($"DestructionArea/DestructionPoly")
@@ -19,8 +24,3 @@ func _physics_process(delta):
 	get_input()
 	rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
-
-func _on_drill_detection_body_entered(body: Node) -> void:
-	if body.is_in_group("Player"): return
-	object_hit = body
-	drill()
--- terrain.gd
+++ terrain.gd
@@ -1,60 +1,47 @@
 extends Node2D
 
-@onready var terrain = $Destructible
-@onready var ColPol = preload("res://map.tscn")
-@export var map: PackedScene
 
-func _ready() -> void:
-	var loaded_map = ColPol.instantiate()
-	add_child(loaded_map)
-	var polygon_list = loaded_map.get_children().filter(func(node): return node is Polygon2D)
-	for polygon in polygon_list:
-		new_col_pol(polygon)
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Destructible/CollisionPolygon2D.polygon = $Rock.polygon
 
-func clip(poly):
+func clip_cur_island(drill_poly, island, collision):
 	var offset_poly = Polygon2D.new()
-	offset_poly.polygon = poly.global_transform * (poly.polygon)
-	
-	var colpol_list = terrain.get_children().filter(func(node): return node is CollisionPolygon2D)
-	for colpol in colpol_list:
-		var curr_poly = colpol.get_node("Polygon2D")
-		var res = Geometry2D.clip_polygons(curr_poly.polygon, offset_poly.polygon)
+	offset_poly.global_position = Vector2.ZERO
+	var new_values = []
+	for point in drill_poly.polygon:
+		new_values.append((drill_poly.global_transform * point))
+	offset_poly.polygon = PackedVector2Array(new_values)
+	var res = Geometry2D.clip_polygons(island.polygon, offset_poly.polygon)
+	# Deletes node if the clip covers all of it. Otherwise sets node polygon.
+	if res.is_empty():
+		island.call_deferred("queue_free")
+		collision.call_deferred("queue_free")
+	else:
+		island.polygon = res[0]
+		collision.set_deferred("polygon", res[0])
+	return res
 
-		#If res empty, the explosion will consume the polygon
-		if res.is_empty():
-			colpol.call_deferred("queue_free")
-		else:
-			curr_poly.polygon = res[0]
-			colpol.set_deferred("polygon", res[0])
-				
-			# If res larger than 1, new islands will be created
-			if res.size() > 1:
-					for i in range(1, len(res)):
-						var island := Polygon2D.new()
-						island.polygon = res[i]
-						new_col_pol(island)
+# Adds Polygon 2D as child of terrain and 
+# collision polygon as child of destructible
+func split_terrain(islands):
+	for i in range(1, len(islands)):
+			var new_island := Polygon2D.new()
+			new_island.polygon = islands[i]
+			self.add_child(new_island)
+			var new_collision_polygon := CollisionPolygon2D.new()
+			new_collision_polygon.polygon = new_island.polygon
+			new_collision_polygon.add_to_group("Destructibles")
+			new_island.set_deferred("color", Color(0.333, 0.173, 0.043))
+			$Destructible.call_deferred("add_child", new_collision_polygon)
 
-	offset_poly.queue_free()
-
-func new_col_pol(polygon):
-	var ColPol = ColPol.instantiate()
-	ColPol.polygon = polygon.polygon
-	terrain.add_child(ColPol)
-## Called when the node enters the scene tree for the first time.
-#func _ready():
-	#$Destructible/CollisionPolygon2D.polygon = $Rock.polygon
-#
-#
-#func clip(poly):
-	#var offset_poly = Polygon2D.new()
-	#offset_poly.global_position = Vector2.ZERO
-	#
-	#var new_values = []
-	#for point in poly.polygon:
-		#new_values.append((poly.global_transform * point))
-	#offset_poly.polygon = PackedVector2Array(new_values)
-	#
-	#var res = Geometry2D.clip_polygons($Rock.polygon, offset_poly.polygon)
-	#
-	#$Rock.polygon = res[0]
-	#$Destructible/CollisionPolygon2D.set_deferred("polygon", res[0])
+func clip(drill_poly):
+	var all_islands = self.get_children().filter(func(node): return node is Polygon2D)
+	var all_collisions = $Destructible.get_children().filter(func(node): return node is CollisionPolygon2D)
+	for i in range(0, len(all_islands)):
+		#Does the clip of current island
+		var result_islands = clip_cur_island(drill_poly, all_islands[i], all_collisions[i])
+		
+		#If result of clip leaves more than one island, create new island(s)
+		if result_islands.size() > 1:
+			split_terrain(result_islands)
